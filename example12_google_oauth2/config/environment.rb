d1ad47d8abdb7051b55566dab27914cf37178a65
Karega K. Scott <kkscott@outlook.com>
2021-05-14 11:56:13 -0500

    Architectural and development migration

--- example12_google_oauth2/config/environment.rb
+++
@@ -1,8 +0,0 @@
-# Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-# Initialize the Rails application.
-AccountManager::Application.initialize!
-
-#Rails.logger = Logger.new(STDOUT)
-#Rails.logger.level = 0
--- example9/app/controllers/home_controller.rb
+++
@@ -1,8 +0,0 @@
-class HomeController < ApplicationController
-  layout "home" #Overrides layout inherited from base controller
-  
-  # Single Page Application (SPA)
-  def index
-  end # index
-  
- end # HomeController
