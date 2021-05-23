b9a50e8d8f4647b289a9120bbb4bab3fcc1d0469
Karega K. Scott <kkscott@outlook.com>
2021-05-23 11:51:32 -0500

    Comments and function updates.

--- webserver.js
+++ webserver.js
@@ -1,8 +1,6 @@
 // https://nodejs.org/docs/latest-v15.x/api/http2.html
 const http = require('http');
-
 const querystring = require('querystring');
-
 const path = require('path');
 const fs = require('fs');
 
@@ -23,54 +21,60 @@ const pagenotfound = '<html><body><h1>Account Manager page not found.</body></ht
 //
 // AM_TENANT_ID='<?>' AM_CLIENT_ID='<?>' AM_CLIENT_SECRET='<?>' node [inspect] webserver.js
 //
-const server = http.createServer((req,res) => {
-	console.log(`${req.method.toUpperCase()}: ${req.url}`);
+const server = http.createServer((request,response) => {
+	console.log(`${request.method.toUpperCase()}: ${request.url}`);
 
 	debugger; // https://bit.ly/2SdN0eY
 
-	if(indexService(req,res)) return;
-	if(callbackService(req,res)) return;
-	if(staticFileService(req,res)) return;
+	if(indexService(request,response)) return;
+	if(mircosoftCallbackService(request,response)) return;
+	if(staticFileService(request,response)) return;
 
-	res.statusCode = 404;
-	res.setHeader('Content-Type', 'text/html');
-	res.end(pagenotfound);
+	response.statusCode = 404;
+	response.setHeader('Content-Type', 'text/html');
+	response.end(pagenotfound);
 });
 
-function readFileContent(res, file, contentType) {
-	res.setHeader('Content-Type', contentType);
+/*
+ * Contents of the file are sent to the client.
+ */
+function readFileContent(httpResponse,file,contentType) {
+	httpResponse.setHeader('Content-Type', contentType);
 
 	fs.promises.readFile(file, {encoding: 'utf8'})
 		.then((data) => {
-			res.statusCode = 200;
-			res.end(data);
+			httpResponse.statusCode = 200;
+			httpResponse.end(data);
 		})
 		.catch((err) => {
-			res.statusCode = 404;
-			res.end();
+			httpResponse.statusCode = 404;
+			httpResponse.end();
 		});
 }
 
-function staticFileService(req,res) {
-	let file = path.join(publicFolder, req.url);
+/*
+ * Controls view access on images, javascripts, stylesheets content.
+ */
+function staticFileService(httpRequest,httpResponse) {
+	let file = path.join(publicFolder,httpRequest.url);
 	let extname = path.extname(file);
 
 	let contentType =  'text/plain';
 	switch(extname) {
 		case '.js':
-			readFileContent(res,file,'text/javascript');
+			readFileContent(httpResponse,file,'text/javascript');
 			break;
 
 		case '.ico':
-			readFileContent(res, file, 'image/x-icon');
+			readFileContent(httpResponse,file,'image/x-icon');
 			break;
 
 		case '.png':
-			readFileContent(res, file, 'image/png');
+			readFileContent(httpResponse,file,'image/png');
 			break;
 
 		case '.css':
-			readFileContent(res, file, 'text/css');
+			readFileContent(httpResponse,file,'text/css');
 			break;
 
 		default:
@@ -80,60 +84,71 @@ function staticFileService(req,res) {
 	return true;
 }
 
-function indexService(req,res) {
-	if(req.url != '/'  && req.url != '/index') return false;
+/*
+ * Controls view access on home page.
+ */
+function indexService(httpRequest,httpResponse) {
+	if(httpRequest.url != '/'  && httpRequest.url != '/index') return false;
 
-	let file = path.join(publicFolder, '/index.html');
-	readFileContent(res, file, 'text/html');
+	let file = path.join(publicFolder,'/index.html');
+	readFileContent(httpResponse,file,'text/html');
+
 	return true;
 }
 
 /*
-	Javascript console example. F12 button.
+ * Callback service used with Microsoft Graph API. 
+ *
+ * Javascript console example. F12 button on browser.
+ * NOTE: Fiddler, JSFiddle and Postman are the same. 
+ *
+ * clear();
+ * fetch('http://localhost:1270/callback', {
+ * 		method: 'POST',
+ * 		headers: { 
+ * 			'Content-Type': 'application/json'
+ * 		},
+ * 		body: JSON.stringify({'name': 'Karega K Scott'})
+ * })
+ * .then(response => response.json())
+ * .then((data) => { console.log(data);})
+ */
+function microsoftCallbackService(httpRequest,httpResponse) {
+	if(httpRequest.url != '/microsoft/callback' || httpRequest.method != 'POST') return false;
 
-	clear();
-	fetch('http://localhost:1270/callback', {
-  		method: 'POST',
-  		headers: { 
-  			'Content-Type': 'application/json'
-  		},
-  		body: JSON.stringify({'name': 'Karega K Scott'})
-	})
-	.then(response => response.json())
-	.then((data) => { console.log(data);})
-*/
-function callbackService(req,res) {
-	if(req.url != '/callback' || req.method != 'POST') return false;
+	httpRequest.body = [];
 
-	req.body = [];
-
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/json');
+	httpResponse.statusCode = 200;
+	httpResponse.setHeader('Content-Type', 'text/json');
 	
-	req.on('data', (chuck) => { req.body.push(chuck); });
-	req.on('end', () => { 
-		req.body = querystring.parse(Buffer.concat(req.body).toString()); 
+	httpRequest.on('data', (chuck) => { httpRequest.body.push(chuck); });
+	httpRequest.on('end', () => { 
+		httpRequest.body = Buffer.concat(httpRequest.body).toString());
+		httpRequest.body = querystring.parse(httpRequest.body);
 
-		microsoftTokenRequest(req,res);
+		microsoftAccessTokenService(httpResponse,httpRequest.body.code);
 	});
-	req.on('error', (err) => { res.end(JSON.stringify({err: err})); });
+	httpRequest.on('error', (err) => { httpResponse.end(JSON.stringify({err: err})); });
 	
 	return true;
 }
 
-function microsoftTokenRequest(req,res) {
-	const redirectUri = querystring.escape(`${process.env.AM_SCHEMA}://${hostname}:${port}/callback`);
+/*
+ * Gets a token used to access the Microsoft Graph API.
+ */
+function microsoftAccessTokenService(httpResponse,code) {
+	const redirectUri = querystring.escape(`${process.env.AM_SCHEMA}://${hostname}:${port}/microsft/callback`);
 
 	const formData = querystring.stringify({
 		client_id: `${process.env.AM_CLIENT_ID}`,
 		scope: 'user.read%20mail.read',
-		code: `${req.body.code}`,
+		code: `${code}`,
 		redirect_uri: `${redirectUri}`,
 		grant_type: 'authorization_code',
 		client_secret: `${process.env.AM_CLIENT_SECRET}`
 	});
 
-	const client = http.request({
+	const httpRequest = http.request({
 		host: 'login.microsoftonline.com',
 		method: 'POST',
 		path: `/${process.env.AM_TENANT_ID}/oauth2/v2.0/token`,
@@ -142,14 +157,15 @@ function microsoftTokenRequest(req,res) {
 			'Content-Length': Buffer.byteLength(formData)
 		}});
 
-	client.end(formData);
-
-	req.socket.on('data', (chuck) => { req.token = chuck.toString()});
-	req.socket.on('end', () => {
-		res.end(JSON.stringify(req.token));
+	httpRequest.on('response', (response) => {
+		// send client the response data
+		httpResponse.end(JSON.stringify(response));
 	});
+
+	httpRequest.end(formData);
 }
 
+// Starts the Account Manager simple web portal 
 server.listen(port, hostname, () => {
 	console.log(`Account Manager Server running on http://${hostname}:${port}`);
 });
