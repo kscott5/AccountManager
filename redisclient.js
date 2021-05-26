4da1c6c3ece10676ab7c98cda2b06eb93d92176a
Karega K. Scott <kkscott@outlook.com>
2021-05-26 11:17:17 -0500

    Creates client that wraps net.Socket. An example is in this file.

--- redisclient.js
+++ redisclient.js
@@ -1,46 +1,88 @@
-// Telnet and redisclient.js are similar. Both send simple commands that redis understands.
-// 
-// https://redis.io/commands
-//
-// copy and paste this script into node.
-//
-// Usage: NOTE carriage returns \r and linefeeds \n commits the request.
-//
-// redis.write(`set 'redis' 'client'\r\n`);
-// redis.data;
-// redis.error;
-//
-// redis.write(`get 'redis'\r\n`);
-// redis.data;
-// redis.error;
-//
-// redis.write(`cleint nfo`\r\n);
-// redis.data;
-// redis.error;
+/*	Telnet and redisclient.js are similar. Both send simple commands that redis understands.
+	
+	https://redis.io/commands
+
+	**Copy and Paste, below is the actual redis connection.**
+
+	const net = require('net');
+
+	socket = new net.Socket({
+		allowHalfOpen: true,
+		readable: false,
+		writable: true
+	});
+
+	socket.setEncoding('utf8');	
+	socket.connect({port: options.port || 6379}); // opens the connection
+
+	socket.on('data',(data)=>{
+		//save information in data after socket.write.
+	});
+	socket.on('error',(error)=>{
+		//report what happens after error with socket.write.
+	});
+
+	socket.setEncoding('utf8');
+	socket.connect({port: 6379});
+
+	// Writes requires both carriage returns \r and linefeeds \n with redis.
+	socket.write(`set 'redis' 'client'\r\n`);
+	socket.write(`get 'redis'\r\n`);
+	socket.write(`cleint nfo`\r\n);
+
+	socket.end(); // socket.connect
+
+	**yank and paste, above is the actual redis connection.**
+*/
 
 const net = require('net');
-const stream = require('stream');
 
-const redis = new net.Socket({allowHalfOpen: true, readable: true, writable: true});
+/**
+ * Creates new redis client with net.Socket
+ */
+function RedisClient(options) {
+	if(!this instanceof RedisClient) {
+		return new RedisClient(options);
+	}
+	
+	/* yank and paste, below is the actual redis connection. */ 
+	this.socket = new net.Socket({
+		allowHalfOpen: options.allowHalfOpen || true,
+		readable: options.readable || false,
+		writable: optinos.writable || true
+	});
 
-redis.on('ready', () => {
-	console.log(`Connection with redis: done`);
-});
-redis.on('data', (data) => {
-	console.log(`Save redis data: done`);
-	redis.data = data.split(`\r\n`)[1];
-	redis.error = '';
-});
-redis.on('error', (error) => {
-	console.log(`Save redis error: done `);
-	redis.error = error;
-	redis.data = '';
-});
-redis.on('close', ()=> {
-	console.log(`Close redis connection: done`);
-});
+	this.port = options.port || 6379;
+	this.prototype.send = send;
+}
+	
 
-redis.setEncoding('utf8');
-redis.connect({port:6379});
+/**
+ * Send redis the command with net.Socket.write.
+ */
+function send(command, callback) {
+	if(!command && !callback instanceof Function) return false;
 
+	// regular expression finds carriage returns \r and line feeds \n
+	if(!command.match(`(\\r\\n|\\n\\r`) /*not found*/) 
+		command = command.concat('\r\n');
+	
+	this.socket.setEncoding('utf8');	
+	this.socket.connect({port: this.port}); // opens the connection with specific port
+
+	this.socket.off('data');	
+	this.socket.on('data', (data) => {
+		console.log(`Save redis data: done`);
+		callback(null,data);	
+	});
+
+	this.socket.off('error');
+	this.socket.on('error', (error) => {
+		console.log(`Save redis error: done `);
+		callback(error,null);
+	});
+
+	this.socket.end(command, callback); // is this.socket.write then this.socket.end.
+
+}
 
