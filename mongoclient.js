162eb79c79f9fe002b67bb11258ba9a9806cb67e
Karega K. Scott <kkscott@outlook.com>
2021-05-28 19:08:18 -0500

    Private key and public certificate verification.

--- mongoclient.js
+++ mongoclient.js
@@ -16,19 +16,6 @@ console.logFile = (data)=>{
 
 console.logFile(`Mongo client logging`);
 
-// Self-signed cert is its own certificate authority
-const ca = process.env.DEFAULT_CERTICATE_AUTHORITY || 'X509 CERTICATE';
-const keyPEMFile = path.resolve(process.env.PRIVATE_CERT_PEM_FILE || '');
-const certPEMFile = path.resolve(process.env.PUBLIC_CERT_PEM_FILE || '');
-
-// public self signed X509 certificate PEM key
-let cert = fs.readFileSync(`${certPEMFile}`);
-if(!cert) console.logFile('Error reading public certificate. Secure socket not ready.');
-
-// private certificate PEM key
-let key = fs.readFileSync(`${keyPEMFile}`);
-if(!key) console.logFile('Error reading private certificate. Secure socket not ready.');
-
 // Create a new client socket
 let socket = new net.Socket({
 	allowHalfOpen: true,
@@ -48,41 +35,59 @@ socket.on('error',(error)=>{
 	console.logFile(`socket.on error: ${error}`);
 });
 
-// Make the client socket more secure with TLS/SSL encryption
-let secureSocket= new tls.TLSSocket(socket, {
-	enableTrace: true,
-	isServer: false,
-	requestCert: false,
-	requestOCSP: true,
-	ca: ca,
-	cert: cert,
-	key: key
-});
+let exportList = { MongoClient: socket };
 
-secureSocket.on('keylog',(line)=>{
-	console.logFile(`secure socket.on keylog: ${line}`);
-});
-secureSocket.on('OCSPResponse',(response)=>{
-	console.logFile(`secure socket.on OCSPResponse: ${response}`);
-});
-secureSocket.on('secureConnect',()=>{
-	console.logFile(`secure socket.on secure connect: done`);
-});
-secureSocket.on('session',(session)=>{
-	console.logFile(`secure socket.on session: ${session}`);
-});
-secureSocket.on('ready',()=>{
-	console.logFile(`secure socket.on ready: use read/write now `);
-});
-secureSocket.on('data',(data)=>{
-	console.logFile(`secure socket.on data: ${data}`);
-});
-secureSocket.on('error',(error)=>{
-	console.logFile(`secure socket.on error: ${error}`);
-});
+// Self-signed cert is its own certificate authority
+const ca = process.env.DEFAULT_CERTICATE_AUTHORITY || 'X509 CERTICATE';
+const keyPEMFile = path.resolve(process.env.PRIVATE_CERT_PEM_FILE || '');
+const certPEMFile = path.resolve(process.env.PUBLIC_CERT_PEM_FILE || '');
 
+// public self signed X509 certificate PEM key
+let cert = fs.readFileSync(`${certPEMFile}`);
+
+// private certificate PEM key
+let key = fs.readFileSync(`${keyPEMFile}`);
+
+if(cert && key) {
+	// Make the client socket more secure with TLS/SSL encryption
+	let secureSocket= new tls.TLSSocket(socket, {
+		enableTrace: true,
+		isServer: false,
+		requestCert: false,
+		requestOCSP: true,
+		ca: ca,
+		cert: cert,
+		key: key
+	});
+
+	secureSocket.on('keylog',(line)=>{
+		console.logFile(`secure socket.on keylog: ${line}`);
+	});
+	secureSocket.on('OCSPResponse',(response)=>{
+		console.logFile(`secure socket.on OCSPResponse: ${response}`);
+	});
+	secureSocket.on('secureConnect',()=>{
+		console.logFile(`secure socket.on secure connect: done`);
+	});
+	secureSocket.on('session',(session)=>{
+		console.logFile(`secure socket.on session: ${session}`);
+	});
+	secureSocket.on('ready',()=>{
+		console.logFile(`secure socket.on ready: use read/write now `);
+	});
+	secureSocket.on('data',(data)=>{
+		console.logFile(`secure socket.on data: ${data}`);
+	});
+	secureSocket.on('error',(error)=>{
+		console.logFile(`secure socket.on error: ${error}`);
+	});
+
+	exportList.MongoSecureClient = secureSocket;
+} else {
+	console.logFile(`Private key and public certificate are not available.`);
+}
 
 //socket.connect({hostname: localhost, port: 27017}); // open the connection
 //socket.write(`{'command': {'find': {'ns': 'dbname.collectionname'}}}`); // BSON string required
 
-module.exports = {MongoSecureClient: secureSocket, MongoClient: socket};
+module.exports = exportList;
