f8ab72780b508e97508b974e73b8c2fd34690bdb
Karega K. Scott <kkscott@outlook.com>
2021-05-30 21:07:37 -0500

    Complex and possible

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,37 @@
-console.log('fetch data');
+window.manager = {
+	activeApi: 'Select',
+	apis: {}
+};
+
+function activateSelection() {
+	window.activeDialog.close();
+
+	let api = window.manager.apis[window.manager.activeApi];
+	api.me();
+}
+
+// This script block called after the DOM load is complete.
+(function() {
+	let appName = document.querySelector('#appName');
+	let library = document.querySelector('#library');
+	let access = document.querySelector('#access');
+	let main = document.querySelector('#main');
+
+	library.onchange = function() {
+		let selection = library.value.split(':');
+		let clientName = selection[0], clientId = selection[1];
+
+		if(clientName == 'Microsoft') {
+			window.manager.activeLibrary = clientName ;
+
+			window.manager.apis[clientName] = 
+				window.manager.apis[clientName] || 
+				new MicrosoftGraph(appName.textContent,clientId);
+		}
+		
+		let api = window.manger.apis[window.manager.activeApi];
+		if(!api.ready) {
+			api.login();
+		}
+	};
+})();
